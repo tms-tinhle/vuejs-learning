d98a4e6a97ea15b6a5dda18ae9685e76bc073b8b
tms-tinhle <tinh.le@tomosia.com>
2025-04-18 12:26:27 +0700

    feat: exercise2

---
+++ week-1/src/components/ChildCounterComponent.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+  import {ref, watch} from 'vue';
+
+  const props = defineProps<{
+    start: number
+  }>();
+
+  const emit = defineEmits<{
+    (e: 'updateCount', newValue: number): void
+  }>();
+
+  const count = ref(props.start);
+
+  watch(() => props.start, (newValue) => {
+    count.value = newValue;
+  });
+
+  const onIncrease = () => {
+    count.value++;
+    emit('increase', count.value);
+  };
+  const onDecrease = () => {
+    count.value--;
+    emit('decrease', count.value);
+  };
+
+</script>
+
+<template>
+  <div class="container">
+    <button @click="onIncrease">Increase</button>
+    <button @click="onDecrease">Decrease</button>
+    <span>{{ count }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/styles/components/ChildCounterComponent.scss';
+</style>
+
+
---
+++ week-1/src/views/ExerciseTwoView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import ChildCounter from '../components/ChildCounterComponent.vue'
+
+const count = ref(100)
+
+const handleUpdateCount = (newValue: number) => {
+  count.value = newValue
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2>Parent Component</h2>
+    <p>Current count from child: {{ count }}</p>
+
+    <ChildCounter :start="count" @updateCount="handleUpdateCount" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/styles/exercise-two.scss';
+</style>
+
+
