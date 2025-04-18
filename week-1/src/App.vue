5b41d4a842a169603be7df08a7bede36f1ee6ca1
tms-tinhle <tinh.le@tomosia.com>
2025-04-18 10:33:42 +0700

    feat: exercise6

--- week-1/src/App.vue
+++ week-1/src/App.vue
@@ -1,70 +1,28 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+const route = useRoute();
+import { NavItems } from '@/constant/navItemsConstant';
+
 </script>
 
 <template>
-
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink
+          v-for="item in NavItems"
+          :key="item.path"
+          :to="item.path"
+          :class="{ active: route.path === item.path }"
+        >
+          {{ item.label }}
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style lang="scss" scoped>
+@use '../src/assets/_app.scss';
 </style>
---
+++ week-1/src/constant/navItemsConstant.ts
@@ -0,0 +1,5 @@
+export const NavItems = [
+  { path: '/bai5.1', label: 'Bài 1:' },
+  { path: '/bai5.2', label: 'Bài 2:' },
+  { path: '/bai5.3', label: 'Bài 3:' },
+];
