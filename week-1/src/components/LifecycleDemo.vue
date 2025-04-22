19326a86c0c4ed429421594c8dc7d49fd33898a2
tms-tinhle <tinh.le@tomosia.com>
2025-04-22 08:15:36 +0700

    feat: exercise1

---
+++ week-1/src/components/LifecycleDemo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Lifecycle Demo Component</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+
+onBeforeMount(() => {
+  console.log('onBeforeMount')
+})
+
+onMounted(() => {
+  console.log('onMounted')
+})
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
+})
+
+onUpdated(() => {
+  console.log('onUpdated')
+})
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount')
+})
+
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
+</script>
+
