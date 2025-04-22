180b1affc1291910ec17d87ae8983d7b533820cf
tms-tinhle <tinh.le@tomosia.com>
2025-04-22 10:30:29 +0700

    feat: exercise2

---
+++ week-1/src/views/ExerciseTwoView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <GCountComponent/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GCountComponent from '@/components/GCountComponent.vue';
+
+</script>
+
+<style lang="scss" scoped>
+  @use '../assets/styles/exercise-one.scss'
+</style>
