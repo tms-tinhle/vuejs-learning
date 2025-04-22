d8c7c3e2187befa700bf85159309b3be847b0888
tms-tinhle <tinh.le@tomosia.com>
2025-04-22 08:30:31 +0700

    feat: exercise2

---
+++ week-1/src/components/Clock.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Bây giờ là: {{ currentTime }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const currentTime = ref('')
+let intervalId: number | null = null
+
+function updateTime() {
+  const now = new Date()
+  currentTime.value = now.toLocaleTimeString()
+}
+
+onMounted(() => {
+  updateTime()
+  intervalId = setInterval(updateTime, 1000)
+  console.log('Clock started')
+})
+
+onUnmounted(() => {
+  if (intervalId !== null) {
+    clearInterval(intervalId)
+  }
+  console.log('Clock stopped')
+})
+</script>
---
+++ week-1/src/views/ExerciseTwoView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <button class="container__button" @click="show = !show">
+      {{ show ? 'Hiden' : 'Show' }} Clock
+    </button>
+
+    <Clock v-if="show" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Clock from '../components/Clock.vue'
+
+const show = ref(true)
+</script>
+
+<style scoped lang="scss">
+  @use '../assets/styles/_exercise-one.scss'
+</style>
