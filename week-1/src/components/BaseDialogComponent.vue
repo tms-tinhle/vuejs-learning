2db42457c48dc05d501624af28ab281e611324f9
tms-tinhle <tinh.le@tomosia.com>
2025-04-22 10:57:04 +0700

    feat: exercise2

---
+++ week-1/src/components/BaseDialogComponent.vue
@@ -0,0 +1,22 @@
+<!-- src/components/BaseDialog.vue -->
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <header class="container__header">
+        <slot name="header" />
+      </header>
+
+      <main class="container__body">
+        <slot />
+      </main>
+
+      <footer class="container__footer">
+        <slot name="footer" />
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/base-dialog';
+</style>
---
+++ week-1/src/views/ExerciseTwoView.vue
@@ -0,0 +1,22 @@
+<template>
+  <BaseDialogComponent>
+    <template #header>
+      <h3>Thông báo</h3>
+    </template>
+
+    Đây là nội dung chính của dialog.
+
+    <template #footer>
+      <button @click="onClose">Đóng</button>
+    </template>
+  </BaseDialogComponent>
+</template>
+
+<script setup lang="ts">
+import BaseDialogComponent from '../components/BaseDialogComponent.vue'
+
+const onClose = () => {
+  alert('Dialog sẽ đóng lại')
+}
+
+</script>
